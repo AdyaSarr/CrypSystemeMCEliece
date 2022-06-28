d403616379c97500a7dfbc06e974b02e2d6ed14f
AdyaSarr <sarradya2@gmail.com>
2022-06-28 08:59:18 +0000

    quelques modif

--- tableAntiLog.py
+++ tableAntiLog.py
@@ -6,16 +6,14 @@ Created on Thu Jun  9 17:55:34 2022
 @author: root
 """
 import parametresFixes
-import representationExponentielle
-
-AntiLog = representationExponentielle.Exp
+import tableLog
 
 def anti_log_element(index, poly_primitif):
     elemnt = index%(parametresFixes.q - 1)
     j = 0
-    for i in range(1, (1<<parametresFixes.puissance)-1):
-        if AntiLog[i] == (1<<elemnt)^parametresFixes.primitif[1]:
+    for i in range(len(tableLog.Log)):
+        if elemnt == tableLog.Log[i]:
             j = i
             break
-    return AntiLog[j]
+    return j
 print(anti_log_element(7, parametresFixes.primitif[1]))
