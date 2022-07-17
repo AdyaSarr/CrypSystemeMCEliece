b1b722524eb72f9d434027f68bfb265b2a89d047
AdyaSarr <sarradya2@gmail.com>
2022-07-17 21:21:35 +0000

    Operations sur les matrices

--- matrix.py
+++ matrix.py
@@ -82,8 +82,8 @@ class matrix:
         return matreturn
     
     def column(self,ind): #renvoie le ind^ème vecteur colonne
-        matreturn=matrix(self.nl,1)
-        for i in range(self.nl):
+        matreturn=matrix(self.nbr_row,1)
+        for i in range(self.nbr_row):
             matreturn[i][0]=self[i][ind]
         return matreturn
     
@@ -91,6 +91,111 @@ class matrix:
         matreturn=matrix(1,self.nbr_col)
         for i in range(self.nbr_col):
             matreturn[0][i]=self[ind][i]
+        return matreturn
+    
+    def __add__(self,other):#permet d'ajouter deux matrice de meme taille
+        if not isinstance(other,matrix):
+            return ("error : S'il vous plait entré une matrice")
+        if (self.nbr_row != other.nbr_row or self.nbr_col != other.nbr_col):
+            return ("error : Les matrices doivent avoir les memes dimensions")
+        mat = matrix(self.nbr_row,self.nbr_col)
+        for i in range(self.nbr_row):
+            for j in range(self.nbr_col):
+                mat[i][j]=int(self[i][j]+other[i][j])%2
+        return mat
+
+    def __sub__(self,other):
+        if not isinstance(other,matrix):
+            return ("error : S'il vous plait entré une matrice")
+        if (self.nbr_row != other.nbr_row or self.nbr_col != other.nbr_col):
+            return ("error : Les matrices doivent avoir les memes dimensions")
+        matreturn = matrix(self.nbr_row,self.nbr_col)
+        for i in range(self.nbr_row):
+            for j in range(self.nbr_col):
+                matreturn[i][j]=int(self[i][j]-other[i][j])%2
+        return matreturn
+    
+    def __mul__(self,other):#la multiplication de  deux matrices
+        matreturn = matrix(self.nbr_row,self.nbr_col)         
+        if isinstance(other,matrix):
+            if (self.nbr_col != other.nbr_row):
+                return ("error: les dimensions ne sont pas bonne")           
+            for i in range(self.nbr_row):
+                for j in range(self.nbr_col):
+                    x=0
+                    for ind in range(self.nbr_col):
+                        x=int(x+self[i][ind]*other[ind][j])%2
+                    matreturn[i][j]=x
+        else:
+            for i in range(self.nbr_row):
+                for j in range(self.nbr_col):
+                    matreturn[i][j]=int(other*self[i][j])%2
+        return matreturn
+    
+    def __pow__(self,exposant):   #la puissance est définie pour les exposants entiers relatifs
+        if not isinstance(exposant,int):
+            return ("error : l'exposant doit etre un entier relatif")
+        if self.nbr_row != self.nbr_col:
+            return ("error : ce n'est pas une matrice carrée")
+        if exposant==0:
+            matreturn = matrix(self.nbr_row,self.nbr_col)
+            matreturn.identity()           
+            return matreturn
+        if exposant>0:
+            matreturn = matrix(self.nbr_row,self.nbr_col)
+            matreturn.identity()
+            for i in range(exposant):
+                matreturn = matreturn*self
+            return matreturn
+        if exposant==-1:
+            if self.nbr_row!=self.nbr_col:
+                return("error : tu dois inversé une matrice carrée")
+            ident=matrix(self.nbr_row,self.nbr_col)
+            ident.identity()           
+            matinter=self.augment_c(ident)        
+            mat1=matinter.gauss_jordan()
+            matreturn=mat1.slice_c(self.nbr_col,mat1.nbr_col+1)
+            return matreturn
+         
+        if exposant<-1:
+            return (self**(-1))**(-exposant)
+    
+    def gauss_jordan(self): #renvoie la matrice échelonnée réduite
+        line, col = self.nbr_row, self.nbr_col
+        i, j = 0, 0
+        tab = self.matrix[:]  
+        while j < col and i < line:
+            tab.sort(reverse=1)
+            k = i+1
+            max = i
+            while k < line:
+                if ( abs( tab[k][j] ) > abs( tab[max][j] ) ):
+                    max = k
+                k = k+1
+            if tab[max][j] != 0:
+                k = j
+                piv = tab[max][j]
+                while k < col:
+                    tab[max][k] = tab[max][k] * 1. / piv
+                    k = k+1
+                k = 0
+                while k < line:
+                    if k != max:
+                        t = 0
+                        ca = tab[k][j]
+                        while t < col:
+                            tab[k][t] = tab[k][t] - ca * tab[max][t]
+                            t = t+1
+                    k = k+1
+                i = i+1
+            j = j+1   
+        tab.sort(reverse=1)
+        for i in range(self.nbr_row):
+            for j in range(self.nbr_col):
+                if tab[i][j]==int(tab[i][j]):
+                    tab[i][j]=int(tab[i][j])
+        matreturn=matrix(self.nbr_row,self.nbr_col)
+        matreturn.load(tab)
         return matreturn
     
     def matrice_standard(self):
